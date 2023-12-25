9cf5a8b025a7bbcedc58b752f8e6bf66c5cf7926
Samarjeet shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2023-12-25 18:29:14 +0530

    Users are created and encryption is done.

---
+++ backend/data/users.js
@@ -0,0 +1,26 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name : 'Admin User',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name : 'john Doe',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false,
+    }
+    ,
+    {
+        name : 'jane Doe',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false,
+    }
+];
+
+
+export default users;
